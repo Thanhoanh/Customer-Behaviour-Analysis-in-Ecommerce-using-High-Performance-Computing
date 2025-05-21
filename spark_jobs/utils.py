7317e7b18a43d6298343c1beb34f81aa8c775a19
Nguyễn Bảo Cát Minh <catminh0909@gmail.com>
2025-05-21 21:43:48 +0700

    Add files via upload

--- spark_jobs/utils.py
+++ spark_jobs/utils.py
@@ -1,1 +1,32 @@
-a
+# utils.py
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import max, datediff, countDistinct, col, sum as Fsum
+from pyspark.ml.feature import VectorAssembler, MinMaxScaler
+import pyspark.sql.functions as F
+
+def create_spark_session(app_name="RFM_Pipeline"):
+    return SparkSession.builder.appName(app_name).getOrCreate()
+
+def load_data(file_path, spark):
+    return spark.read.csv(file_path, header=True, inferSchema=True)
+
+def compute_rfm(df):
+    snapshot_date = df.agg(max("InvoiceDate")).collect()[0][0]
+    rfm = df.groupBy("Customer ID").agg(
+        datediff(F.lit(snapshot_date), max("InvoiceDate")).alias("Recency"),
+        countDistinct("Invoice").alias("Frequency"),
+        Fsum(col("Quantity") * col("Price")).alias("Monetary")
+    )
+    return rfm.dropna()
+
+def get_preprocessing_stages():
+    assembler = VectorAssembler(inputCols=["Recency", "Frequency", "Monetary"], outputCol="features_raw")
+    scaler = MinMaxScaler(inputCol="features_raw", outputCol="features")
+    return [assembler, scaler]
+
+def preprocess_features(rfm_df):
+    stages = get_preprocessing_stages()
+    from pyspark.ml import Pipeline
+    pipeline = Pipeline(stages=stages)
+    model = pipeline.fit(rfm_df)
+    return model.transform(rfm_df)
